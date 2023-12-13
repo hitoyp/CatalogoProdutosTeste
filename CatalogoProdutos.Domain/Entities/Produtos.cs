1be0b6111c05a91bd42c7eba1d261f303c14e2e4
hitoyp <74926675+hitoyp@users.noreply.github.com>
2023-12-13 20:41:37 -0300

    Add files via upload

---
+++ CatalogoProdutos.Domain/Entities/Produtos.cs
@@ -0,0 +1,27 @@
+﻿namespace CatalogoProdutos.Domain.Entities
+{
+    public class Produtos 
+    {
+        public Produtos(int produtoId, string? nome, int precoDeVenda, string? descricao, int quantidade, string? tipo, DateTime dataCadastro)
+        {
+            ProdutoId = produtoId;
+            Nome = nome;
+            PrecoDeVenda = precoDeVenda;
+            Descricao = descricao;
+            Quantidade = quantidade;
+            Tipo = tipo;
+            DataCadastro = dataCadastro;
+        }
+
+        public int ProdutoId { get; protected set; }
+        public string? Nome { get; private set; }
+        public int PrecoDeVenda { get; private set;}
+        public string? Descricao { get; private set; }
+        public int  Quantidade { get; private set; }
+        public string? Tipo  { get; private set; }
+        public DateTime DataCadastro { get; private set; } 
+
+        
+
+    }
+}
---
+++ CatalogoProdutos.Domain/Interface/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CatalogoProdutos.Domain.Entities;
+
+namespace CatalogoProdutos.Domain.Interface
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produtos>> GetProdutosAsync();
+        Task<Produtos?> GetByIdAsync(int id);
+        Task<Produtos> CreateAsync(Produtos produtos);
+        Task<Produtos> UpdateAsync(Produtos produtos);
+        Task<Produtos> RemoveAsync(Produtos produtos);
+    }
+}
