8e07ab7653f62426d4b4dfe308b6e989eb77e466
hitoyp <74926675+hitoyp@users.noreply.github.com>
2023-12-13 20:37:19 -0300

    Add files via upload

---
+++ CatalogoProdutos.CrossCutting/Ioc/DependencyInjectionApi.cs
@@ -0,0 +1,30 @@
+﻿using CatalogoProdutos.Application.Interface;
+using CatalogoProdutos.Application.Mapping;
+using CatalogoProdutos.Application.Service;
+using CatalogoProdutos.Domain.Interface;
+using CatalogoProdutos.Infrastructure.Context;
+using CatalogoProdutos.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace CatalogoProdutos.CrossCutting.Ioc
+{
+    public static class DependencyInjectionApi
+    {
+        public static IServiceCollection AddInjectionInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("CatalogoProdutoDB");
+            }); 
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+
+            services.AddAutoMapper(typeof(MappingProdutoDTO));
+
+            return services;
+        }
+    }
+}
