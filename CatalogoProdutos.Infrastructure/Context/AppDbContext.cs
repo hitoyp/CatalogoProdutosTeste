c74769632f2668c02348b0d9d6866ef2bb198626
hitoyp <74926675+hitoyp@users.noreply.github.com>
2023-12-13 20:42:55 -0300

    Add files via upload

---
+++ CatalogoProdutos.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CatalogoProdutos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoProdutos.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+
+        public DbSet<Produtos> Produtos { get; set; }
+    }
+}
---
+++ CatalogoProdutos.Infrastructure/Repository/ProdutoRepository.cs
@@ -0,0 +1,48 @@
+﻿using CatalogoProdutos.Domain.Entities;
+using CatalogoProdutos.Domain.Interface;
+using CatalogoProdutos.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoProdutos.Infrastructure.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Produtos> CreateAsync(Produtos produtos)
+        {
+            _context.Add(produtos);
+            await _context.SaveChangesAsync();
+            return produtos;
+        }
+
+        public async Task<Produtos> RemoveAsync(Produtos produtos)
+        {
+            _context.Remove(produtos);
+            await _context.SaveChangesAsync();
+            return produtos;
+        }
+
+        public async Task<Produtos?> GetByIdAsync(int id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produtos>> GetProdutosAsync()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        public async Task<Produtos> UpdateAsync(Produtos produtos)
+        {
+            _context.Update(produtos);
+            await _context.SaveChangesAsync();
+            return produtos;
+        }
+    }
+}
